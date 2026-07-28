497459f34f8229d7ff002e1cbcf49c38fb76cc48
Samson <samsonbraimoh32@gmail.com>
2026-07-28 20:10:26 +0100

    fixed a bug

--- app.js
+++ app.js
@@ -54,78 +54,96 @@ const dailyForcastData = [
 
 const day = document.getElementById("day");
 
-const dailyForcast = dailyForcastData.map( (forecast) =>{
-    return `
-        <div class="dayWrapper" key=${forecast.key}>
-            <div class="day">
-                <p>${forecast.day}</p>
-                <img src=${forecast.weatherCondition} class="weatherEmoji" alt="sunny">
-                <div class="settling">
-                    <p>${forecast.settlingFrom}</p>
-                    <p>${forecast.settlingTo}</p>
+    const displayDailyTemp = (weatherData) => {
+        const dailyForcast = weatherData.daily.time.map((date, index) =>{
+            return `
+                <div class="dayWrapper">
+                    <div class="day">
+                        <p>${new Date(date).toLocaleDateString("en-US", { weekday: "short" })}</p>
+                        <div class="settling">
+                            <p>${weatherData.daily.temperature_2m_min[index]}</p>
+                            <p>${weatherData.daily.temperature_2m_max[index]}</p>
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </div>
-    `
-}       
-).join("");
-day.innerHTML = dailyForcast;
+            `
+        }       
+        ).join("");
+        day.innerHTML = dailyForcast;
+    }
 
-const hourlyForcastData = [
-    {
-        timeInHour: "3 PM",
-        weatherConditionImage: "assets/images/icon-snow.webp",
-        temperature: "63°"
-    },
-    {
-        timeInHour: "4 PM",
-        weatherConditionImage: "assets/images/icon-sunny.webp",
-        temperature: "68°"
-    },
-    {
-        timeInHour: "5 PM",
-        weatherConditionImage: "assets/images/icon-rain.webp",
-        temperature: "68°"
-    },
-    {
-        timeInHour: "6 PM",
-        weatherConditionImage: "assets/images/icon-partly-cloudy.webp",
-        temperature: "63°"
-    },
-    {
-        timeInHour: "7 PM",
-        weatherConditionImage: "assets/images/icon-storm.webp",
-        temperature: "63°"
-    },
-    {
-        timeInHour: "8 PM",
-        weatherConditionImage: "assets/images/icon-sunny.webp",
-        temperature: "68°"
-    },
-    {
-        timeInHour: "9 PM",
-        weatherConditionImage: "assets/images/icon-fog.webp",
-        temperature: "67°"
-    },
-    {
-        timeInHour: "10 PM",
-        weatherConditionImage: "assets/images/icon-rain.webp",
-        temperature: "63°"
-    },
-    {
-        timeInHour: "10 PM",
-        weatherConditionImage: "assets/images/icon-rain.webp",
-        temperature: "63°"
-    },
-    {
-        timeInHour: "10 PM",
-        weatherConditionImage: "assets/images/icon-rain.webp",
-        temperature: "63°"
-    },
-    {
-        timeInHour: "10 PM",
-        weatherConditionImage: "assets/images/icon-rain.webp",
-        temperature: "63°"
+    // const dailyForcast = dailyForcastData.map((forecast) =>{
+    //     return `
+    //         <div class="dayWrapper" key=${forecast.key}>
+    //             <div class="day">
+    //                 <p>${forecast.day}</p>
+    //                 <img src=${forecast.weatherCondition} class="weatherEmoji" alt="sunny">
+    //                 <div class="settling">
+    //                     <p>${forecast.settlingFrom}</p>
+    //                     <p>${forecast.settlingTo}</p>
+    //                 </div>
+    //             </div>
+    //         </div>
+    //     `
+    // }       
+    // ).join("");
+    // day.innerHTML = dailyForcast;
+
+    const hourlyForcastData = [
+        {
+            timeInHour: "3 PM",
+            weatherConditionImage: "assets/images/icon-snow.webp",
+            temperature: "63°"
+        },
+        {
+            timeInHour: "4 PM",
+            weatherConditionImage: "assets/images/icon-sunny.webp",
+            temperature: "68°"
+        },
+        {
+            timeInHour: "5 PM",
+            weatherConditionImage: "assets/images/icon-rain.webp",
+            temperature: "68°"
+        },
+        {
+            timeInHour: "6 PM",
+            weatherConditionImage: "assets/images/icon-partly-cloudy.webp",
+            temperature: "63°"
+        },
+        {
+            timeInHour: "7 PM",
+            weatherConditionImage: "assets/images/icon-storm.webp",
+            temperature: "63°"
+        },
+        {
+            timeInHour: "8 PM",
+            weatherConditionImage: "assets/images/icon-sunny.webp",
+            temperature: "68°"
+        },
+        {
+            timeInHour: "9 PM",
+            weatherConditionImage: "assets/images/icon-fog.webp",
+            temperature: "67°"
+        },
+        {
+            timeInHour: "10 PM",
+            weatherConditionImage: "assets/images/icon-rain.webp",
+            temperature: "63°"
+        },
+        {
+            timeInHour: "10 PM",
+            weatherConditionImage: "assets/images/icon-rain.webp",
+            temperature: "63°"
+        },
+        {
+            timeInHour: "10 PM",
+            weatherConditionImage: "assets/images/icon-rain.webp",
+            temperature: "63°"
+        },
+        {
+            timeInHour: "10 PM",
+            weatherConditionImage: "assets/images/icon-rain.webp",
+            temperature: "63°"
     },
 ]
 const hour = document.getElementById("hour");
@@ -148,22 +166,31 @@ hour.innerHTML = hourlyForcast;
 //     alert("How fa")
 // })
 
-const url = "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&daily=temperature_2m_max,temperature_2m_min&hourly=temperature_2m&current=temperature_2m,relative_humidity_2m,precipitation,wind_speed_10m&past_days=7&wind_speed_unit=mph&temperature_unit=fahrenheit&precipitation_unit=inch" 
+const url = "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&daily=temperature_2m_max,temperature_2m_min,weather_code&hourly=temperature_2m&current=temperature_2m,relative_humidity_2m,precipitation,wind_speed_10m,weather_code&wind_speed_unit=mph&temperature_unit=fahrenheit&precipitation_unit=inch" 
 
+const displayCurrentWeather = (data) => {
+    const currentTemp = document.getElementById("currentTemp");
+    const humid = document.getElementById("humid");
+    const windSpeed = document.getElementById("windSpeed")
+    const precipitation = document.getElementById("precipitation")
+    currentTemp.textContent = `${data.current.temperature_2m}°`
+    windSpeed.textContent  = `${data.current.wind_speed_10m} mph`
+    precipitation.textContent = `${data.current.precipitation} in`
+    humid.textContent = `${data.current.relative_humidity_2m}%`
+}
 
 const getData =  async ()=>{
     try {
     const response =  await  fetch(url);
     const data = await response.json();
-    console.log(data.current)
-    const currentTemp = document.getElementById("currentTemp");
-    currentTemp.textContent = `${data.current.temperature_2m}°`
+    console.log(data)
+        displayCurrentWeather(data);
+        displayDailyTemp(data);
     } 
     catch(err){
         alert("Couldn't reach server");
     }
 }
-
 
 getData();
 
